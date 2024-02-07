3f010fef3507bac41ccb1932d124311d2108d384
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-07 01:23:46 -0800

    upload q2

---
+++ Q2/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class BankAccount
+{
+    public int AccountNumber;
+    public double Balance = 1000;
+
+    public void Deposit(double amount)
+    {
+        Balance = Balance + amount;
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+        Console.WriteLine("Enter your account Number:");
+        account.AccountNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter The amount:");
+        double amount = Convert.ToDouble(Console.ReadLine());
+
+        account.Deposit(amount);
+
+        Console.WriteLine("Account number:" + account.AccountNumber);
+        Console.WriteLine("Update balance:" + account.Balance);
+
+        Console.ReadLine();
+    }
+}
