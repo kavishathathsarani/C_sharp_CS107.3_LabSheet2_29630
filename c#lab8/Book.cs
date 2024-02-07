241e34f70e9d35bd2555d51dde474035482dd00b
_USER_ <_USER_@DESKTOP-2SR3E2E>
2024-02-07 00:53:25 -0800

    Add project files.

---
+++ c#lab8/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_lab8
+{
+    internal class Books
+    {
+        public string Title;
+        public string Author;
+    }
+}
---
+++ c#lab8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_lab8
+{
+    internal class Book
+    {
+        static void Main(string[] args)
+        {
+            Books book = new Books();
+            book.Author = "Arita Desai";
+            book.Title = "Village by the sea";
+
+            Console.WriteLine("Title " + book.Title);
+            Console.WriteLine("Author " + book.Author);
+
+            Console.ReadLine();
+
+        }
+    }
+}
