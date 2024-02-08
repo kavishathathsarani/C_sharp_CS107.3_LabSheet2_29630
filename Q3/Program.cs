0edf98729b713e266b1ec30eac35e12d85160aec
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-08 07:17:36 -0800

    upload labsheet

---
+++ Q3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class TemperatureTracker
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker tracker = new TemperatureTracker();
+
+            tracker.DailyTemperature();
+            Console.WriteLine("\n");
+            tracker.Report();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Q3/TemperatureTracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class TemperatureTrackers
+    {
+        public double[] Temperature = new double[7];
+        public void DailyTemperature()
+        {
+            for (int i = 0; i < 7; i++)
+
+            {
+                Console.Write($"Enter temperature for day {i + 1}: ");
+                Temperature[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        public void Report()
+        {
+            Console.WriteLine("Weekly Temperature Renort ");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {Temperature[1]}°C");
+            }
+        }
+    }
+}
